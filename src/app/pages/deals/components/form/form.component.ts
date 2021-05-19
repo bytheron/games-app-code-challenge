74ed38016d61e66d435f9e62c0f89e843195e39c
Byron <bytheron01@gmail.com>
2021-05-19 14:44:00 +0200

    style: update search field styling

--- src/app/pages/deals/components/form/form.component.ts
+++ src/app/pages/deals/components/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  @Input() formData: any;
 
   constructor() { }
 
